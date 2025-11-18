6800babe964360ec58a18675687f2674c99f1cc4
zeno0104 <jaehun4086@naver.com>
2025-11-18 17:48:28 +0900

    [Update] 시험준비쓰

---
+++ 시험문제준비/js/exam.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector("#btn");
+const addItem = document.querySelector("#addItem");
+
+btn.addEventListener("click", () => {
+  const width = document.querySelector("#width");
+  const height = document.querySelector("#height");
+  const backgroundColor = document.querySelector("#backgroundColor");
+  const color = document.querySelector("#color");
+  const position = document.querySelector("input[name='position']:checked");
+  const content = document.querySelector("#content");
+  const fontSize = document.querySelector("#fontSize");
+
+  console.log(position.value);
+  const p = document.createElement("p");
+  p.style.margin = 0;
+  p.style.width = width.value + "px";
+  p.style.height = height.value + "px";
+  p.style.backgroundColor = backgroundColor.value;
+  p.style.color = color.value;
+  p.style.textAlign = position.value;
+  p.style.fontSize = fontSize.value + "px";
+  p.innerText = content.value;
+  addItem.append(p);
+});
